b2b229b1fd329f33c9b09ef76a318c9d774a47dc
Anton Golubev <antongolubev5@yandex.ru>
2020-07-14 21:44:53 +0300

    Add files via upload

--- Updating Results On Russian Sentiment Datasets/csv2json.py
+++ Updating Results On Russian Sentiment Datasets/csv2json.py
@@ -1,81 +1,11 @@
 import json
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from tqdm import tqdm
-
-from collections import Counter
-import seaborn as sns
-from sklearn.feature_extraction.text import TfidfVectorizer
-from numpy import dot
-from numpy.linalg import norm
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-
-
-def vocab_from_file(directory_path, file_names):
-    vocab = dict()
-    for file_name in file_names:
-        with open(os.path.join(directory_path, file_name), 'r') as f:
-            for line in f:
-                vocab[line.strip().split(', ')[0]] = line.strip().split(', ')[3]
-    return vocab
-
-
-def check_sentiments(words, entities_vocab):
-    """
-    классификация предложения на группы:
-    """
-    neg_words = set([key for key, value in entities_vocab.items() if value == 'negative'])
-    pos_words = set([key for key, value in entities_vocab.items() if value == 'positive'])
-    words = set(words)
-    if len(words.intersection(pos_words)) == 2:
-        return 'pospos'
-    elif len(words.intersection(neg_words)) == 2:
-        return 'negneg'
-    elif len(words.intersection(neg_words)) == len(words.intersection(pos_words)) == 1:
-        return 'posneg'
+import argparse
 
 
 def create_sample(directory_path, contexts, sample_type: str):
-    """
-    создание json-выборки (train/test)
-    contexts - df с сэмплами
-    contexts_file_name - имя исходного csv-файла с данными
-    sample_type - train/test/val выборка
-    """
-    entities_vocab = vocab_from_file(directory_path, ['nouns_person_neg', 'nouns_person_pos'])
-    json_file = open(os.path.join(directory_path, 'rambler2011' + '-' + sample_type + '.json'), 'w')
-    json_contexts = []
-
-    for i in tqdm(range(len(contexts))):
-        sentiment = 0
-        tonal_words_summary = []
-        if contexts.iloc[i]['label'] == 0:
-            tonal_words = contexts.iloc[i]['tonal_word'].split('----MY ARTIFICIAL DELIMITER----')
-        else:
-            tonal_words = contexts.iloc[i]['tonal_word'].split()
-        text = contexts.iloc[i]['text_tok']
-        for word_num in range(len(tonal_words)):
-            text = text.replace(tonal_words[word_num], 'LOCATION' + str(word_num + 1))
-            word2mask = tonal_words[word_num]
-            target_entity = 'LOCATION' + str(word_num + 1)
-            if word2mask not in entities_vocab.keys():
-                sentiment = 'нейтрально'
-            elif entities_vocab[word2mask] == 'positive':
-                sentiment = 'положительно'
-            elif entities_vocab[word2mask] == 'negative':
-                sentiment = 'отрицательно'
-            tonal_words_summary.append({'sentiment': sentiment, 'aspect': 'вообще', 'target_entity': target_entity,
-                                        'tonal_word': word2mask})
-        json_contexts.append(
-            {'opinions': tonal_words_summary,
-             'id': str(i),
-             'text': text})
-    json_file.write(json.dumps(json_contexts, indent=4, ensure_ascii=False))
-
-
-def create_sample_banks(directory_path, contexts, sample_type: str):
     """
     создание json-выборки (train/test)
     contexts - df с сэмплами
@@ -109,13 +39,21 @@ def create_sample_banks(directory_path, contexts, sample_type: str):
 
 
 def main():
-    directory_path = '/home/anton/PycharmProjects/ABSA-BERT-pair'
-    train = pd.read_csv(os.path.join(directory_path, 'news_eval_train_cleaned.csv'), sep='\t')
-    test = pd.read_csv(os.path.join(directory_path, 'news_eval_test_cleaned.csv'), sep='\t')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset",
+                        default=None,
+                        type=str,
+                        required=True,
+                        choices=["romip_2012", "sentirueval_2015_banks", "sentirueval_2015_telecom",
+                                 "sentirueval_2016_banks", "sentirueval_2016_telecom"],
+                        help="Dataset for evaluation.")
+    args = parser.parse_args()
 
-    create_sample_banks(directory_path, test, 'test')
-    create_sample_banks(directory_path, test, 'dev')
-    create_sample_banks(directory_path, train, 'train')
+    train = pd.read_csv(os.path.join('preprocessed_data', args.dataset, 'train.csv'), sep='\t')
+    test = pd.read_csv(os.path.join('preprocessed_data', args.dataset, 'test.csv'), sep='\t')
+
+    for sample, name in [(test, 'test'), (test, 'dev'), (train, 'train')]:
+        create_sample('data/rambler2011_json', sample, name)
 
 
 if __name__ == "__main__":
